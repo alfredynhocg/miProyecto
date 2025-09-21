0567963de993de1d1760add1eac249e829c34b22
Alfredo Callizaya Gutierrez <alfredynho.cg@gmail.com>
2025-09-21 11:30:24 -0400

    feat(home): integrate template layout into Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PostList } from './post-list/post-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: PostList
+	}
+];
